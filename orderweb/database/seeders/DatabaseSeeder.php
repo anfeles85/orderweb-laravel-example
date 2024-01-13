4591c4ed802c655b2f9f732fd33270c3b48a41c9
Andres Escobar <anfelesvillada@gmail.com>
2024-01-12 21:14:52 -0500

    Migraciones y seeders
    Se crean las migraciones de toda la bd, y algunos seeders y factories. También se crean algunos models necesarios.

---
+++ orderweb/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Technician;
+use App\Models\TypeActivity;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        $this->call(RoleSeeder::class);
+        $this->call(CausalSeeder::class);
+        $this->call(ObservationSeeder::class);
+        $this->call(TypeActivitySeeder::class);
+
+        User::factory()->create([
+            'role_id' => 1,
+        ]);
+
+        User::factory(3)->create([
+            'role_id' => 2,
+        ]);        
+
+        Technician::factory()->create([
+            'especiality' => 'Instalación de redes',
+        ]);
+
+        Technician::factory(2)->create([
+            'especiality' => 'Construcción',
+        ]);
+
+        Technician::factory(2)->create([
+            'especiality' => 'Lectura de redes',
+        ]);
+
+        Technician::factory(2)->create();
+    }
+}
