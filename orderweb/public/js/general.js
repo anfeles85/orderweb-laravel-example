cd6cb17ed6b54ef929b015b9aea362c948d4d3db
Andres Escobar <anfelesvillada@gmail.com>
2024-01-31 21:22:02 -0500

    Ajuste datatable
    se ajusta script con lenguaje en español y otras opciones

--- orderweb/public/js/general.js
+++ orderweb/public/js/general.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
-    $('#table_data').DataTable();
+    $('#table_data').DataTable({
+        "pageLength": 10,
+        "lengthChange": false,
+        "language": {
+            "paginate": {
+                "previous": "Anterior",
+                "next": "Sgte"
+            },
+            "search": "Buscar:",
+            "info": "Mostrando _START_ a _END_ de _TOTAL_ registros",
+            "infoEmpty": "No existen registros",
+        }
+    });
 });
 
 function remove() {
