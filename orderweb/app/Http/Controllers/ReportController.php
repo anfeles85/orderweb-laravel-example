56e48b645a6e7808be2435b276582828bd23371b
Andres Escobar <anfelesvillada@gmail.com>
2024-02-19 22:23:16 -0500

    Reportes PDF P1
    se agrega la dependencia dompdf, se crea una template y css para reportes. Se crea ReportController y se crea un reporte de técnicos.

---
+++ orderweb/app/Http/Controllers/ReportController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Technician;
+use Barryvdh\DomPDF\Facade\Pdf;
+use Illuminate\Http\Request;
+
+class ReportController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('reports.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function export_technicians()
+    {
+        $technicians = Technician::all();
+        $data = array(
+            'technicians' => $technicians
+        );
+        $pdf = Pdf::loadView('reports.export_technicians', $data)->setPaper('letter', 'portrait');
+        return $pdf->download('Technicians.pdf');    
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ orderweb/resources/views/reports/index.blade.php
@@ -0,0 +1,28 @@
+@extends('templates.base')
+
+@section('title', 'Reportes')
+
+@section('header', 'Reportes')
+
+@section('content')    
+
+    @include('templates/messages')
+
+    <div class="row">
+        <div class="col-lg-12 mb-4">
+            <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                    <h6 class="m-0 font-weight-bold text-primary">Reporte general de técnicos</h6>
+                </div>
+                <div class="card-body">
+                    <a href="{{ route('reports.technicians') }}" title="pdf" class="btn btn-info btn-block col-lg-3 mb-4">
+                        <i class="fa-solid fa-file-pdf"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection    
+
+
