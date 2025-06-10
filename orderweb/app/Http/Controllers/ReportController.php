f84d727a0e37ee789106d5a57f44d0e5ae22af61
Andres Escobar <anfelesvillada@gmail.com>
2025-06-10 09:47:55 -0500

    reportes P2
    se termina reporte de tecnicos y se crea reporte de actividades por técnico

--- orderweb/app/Http/Controllers/ReportController.php
+++ orderweb/app/Http/Controllers/ReportController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Activity;
 use App\Models\Technician;
 use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Http\Request;
@@ -10,7 +11,8 @@ class ReportController extends Controller
 {
     public function index()
     {
-        return view('reports.index');
+        $technicians = Technician::all();
+        return view('reports.index', compact('technicians'));
     }
 
     /**
@@ -23,8 +25,38 @@ class ReportController extends Controller
             'technicians' => $technicians
         );
 
+        /**
+         * dompdf version 3.x
+         * se debe agregar setOptions
+         */
         $pdf = Pdf::loadView('reports.export_technicians', $data)
-                ->setPaper('letter', 'portrait'); //landscape: horizontal
-        return $pdf->download('technicians.pdf');        
+                ->setPaper('letter', 'portrait')
+                ->setOptions([
+                    'defaultFont'=>'sans-serif', 
+                    'isRemoteEnabled'=>true
+                ]); //landscape: horizontal
+                
+        return $pdf->download('technicians.pdf');
+    }
+
+    /**
+     * reporte que genera el listado de actividades de un técnico
+     */
+    public function export_activities_by_technician(Request $request)
+    {
+        $activities = Activity::where('technician_id', $request['technician_id'])->get();
+                
+        $data = array(
+            'activities' => $activities
+        );
+
+        $pdf = Pdf::loadView('reports.export_activities_by_technician', $data)
+                ->setPaper('letter', 'portrait')
+                ->setOptions([
+                    'defaultFont'=>'sans-serif', 
+                    'isRemoteEnabled'=>true
+                ]); 
+                
+        return $pdf->download('ActivitiesByTechnician-' . $request['technician_id'] . '.pdf');
     }
 }
